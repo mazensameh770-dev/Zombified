0ae3cce6e4fcd63933371d079a7a1c50f33e9b95
MAZEN SAMEH <mazensameh770@gmail.com>
2026-08-10 18:35:50 +0300

    Func

--- Assets/Project/Scripts/Grid/GridTileState.cs
+++ Assets/Project/Scripts/Grid/GridTileState.cs
@@ -8,7 +8,27 @@ public class GridTileState : MonoBehaviour
     public GameObject PlacedTrap { get; private set; }
     public TrapCardData SourceCardData { get; private set; }
 
-    public GridTileState[] neighbors;
+    public GridTileState[] neighbors = new GridTileState[4];
+
+    public GridTileState GetFront()
+    {
+        return (neighbors != null && neighbors.Length > 0) ? neighbors[0] : null;
+    }
+
+    public GridTileState GetBack()
+    {
+        return (neighbors != null && neighbors.Length > 1) ? neighbors[1] : null;
+    }
+
+    public GridTileState GetRight()
+    {
+        return (neighbors != null && neighbors.Length > 2) ? neighbors[2] : null;
+    }
+
+    public GridTileState GetLeft()
+    {
+        return (neighbors != null && neighbors.Length > 3) ? neighbors[3] : null;
+    }
 
     public void SetPlacedTrap(GameObject trap, TrapCardData sourceCardData)
     {
